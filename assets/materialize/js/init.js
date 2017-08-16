da95f37333857af4faf9818eea1254375227c673
Kevin Lidstone <kevin.lidstone@solace.com>
2017-08-15 22:02:40 -0400

    New header

--- assets/materialize/js/init.js
+++ assets/materialize/js/init.js
@@ -3,20 +3,36 @@
 
 		$('.button-collapse').sideNav();
 		$('.parallax').parallax();
-		var options = [
-			{
-				selector: '#index-banner', offset: 1200, callback: function (el) {
-					$("nav").addClass("loaded");
-					$(".navbar-fixed").addClass("loaded");
-				}
-			}
-		];
-		Materialize.scrollFire(options);
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
 
 function addBannerClass() {
-	console.log("Kevin");
 	$(".banner").addClass("loaded");
+}
+
+var last_known_scroll_position = 0;
+var ticking = false;
+
+function doSomething(scroll_pos) {
+	if (scroll_pos == 0) {
+		$("nav").removeClass("loaded");
+		$(".navbar-fixed").removeClass("loaded");
+	} else if (scroll_pos >= 400) {
+		$("nav").addClass("loaded");
+		$(".navbar-fixed").addClass("loaded");
+	}
+}
+
+function addScrollListener() {
+	window.addEventListener('scroll', function (e) {
+		last_known_scroll_position = window.scrollY;
+		if (!ticking) {
+			window.requestAnimationFrame(function () {
+				doSomething(last_known_scroll_position);
+				ticking = false;
+			});
+		}
+		ticking = true;
+	});
 }
